be632d6cbb84c29b9a446fbac44e96f0e5cd34e4
Fabricio Gonzalez <fabrigonzalez171@gmail.com>
2026-06-16 17:06:22 -0300

    Fix: integracion con delivery

--- app/api/mercadopago/webhook/route.ts
+++ app/api/mercadopago/webhook/route.ts
@@ -395,6 +395,8 @@ async function getDeliveryAmount(
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
+                'x-internal-api-key':
+                    process.env.INTERNAL_API_KEY!,
             },
         }
     );
